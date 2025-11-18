ac5b789b2dde4bae0cbadf0bead438bdc3ff4309
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-11-18 10:03:50 -0300

    master

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        for ($id = 1; $id <= 3; $id++) {
+            DB::table('users')->insert([
+                'name' => 'User ' . $id,
+                'email' => 'user_' . $id . '@example.com',
+                'password' => bcrypt("user$id"),
+                'email_verified_at' => now(),
+                'created_at' => now(),
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            ]);
+        }
     }
 }
